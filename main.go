08859280a92ea64fd606fad1e1ca9310c9a4e271
hrn-tmy <mj0829mj0625@icloud.com>
2024-12-03 00:22:31 +0900

    出力のカスタマイズを行う

--- main.go
+++ main.go
@@ -6,10 +6,25 @@ import (
 )
 
 func main() {
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil)).With(slog.String("メッセージ", "共通ログ"))
-
+	// 出力のカスタマイズを行う
+	opt := slog.HandlerOptions{
+		// ログの出力場所を詳述する
+		AddSource: true,
+		// 出力するログレベルを設定
+		Level: slog.LevelWarn,
+		// 追加情報の出力に関する設定
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			if a.Value.String() == "Warn Message" {
+				a.Value = slog.StringValue("WARN MESSAGE")
+			}
+			return a
+		},
+	}
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &opt))
+	// Warn以上を出力するように設定したため出力されない
 	logger.Info("Info Message")
-	// {"time":"2024-12-02T22:39:06.791661+09:00","level":"INFO","msg":"Info Message","メッセージ":"共通ログ"}
+	logger.Warn("Warn Message")
+	// {"time":"2024-12-02T22:29:02.602679+09:00","level":"WARN","source":{"function":"main.main","file":"/Users/hoge/fuga/golang_logs/main.go","line":21},"msg":"WARN MESSAGE"}
 	logger.Error("Error Message")
-	// {"time":"2024-12-02T22:39:06.792175+09:00","level":"ERROR","msg":"Error Message","メッセージ":"共通ログ"}
+	// {"time":"2024-12-02T22:29:02.603285+09:00","level":"ERROR","source":{"function":"main.main","file":"/Users/hoge/fuga/golang_logs/main.go","line":22},"msg":"Error Message"}
 }
