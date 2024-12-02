0294bc07e5bbc50670dcade5c2c719a0bfcebbb7
hrn-tmy <mj0829mj0625@icloud.com>
2024-12-03 00:21:12 +0900

    ログにコンテキストを付け加える

--- main.go
+++ main.go
@@ -6,13 +6,11 @@ import (
 )
 
 func main() {
-	logger := slog.New(slog.NewJSONHandler(os.Stdout,nil))
-	// 上記ログをデフォルトとして指定することができる
-	slog.SetDefault(logger)
-	slog.Info("Info Message")
-	// {"time":"2024-12-02T22:06:41.895091+09:00","level":"INFO","msg":"Info Message"}
-	slog.Warn("Warn Message")
-	// {"time":"2024-12-02T22:06:41.895399+09:00","level":"WARN","msg":"Warn Message"}
-	slog.Error("Error Message")
-	// {"time":"2024-12-02T22:06:41.895403+09:00","level":"ERROR","msg":"Error Message"}
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	logger.Info("Info Message", slog.String("追加メッセージ", "Infoレベルのメッセージです"))
+	logger.Info("Info Message", "追加メッセージ", "Infoレベルのメッセージです")
+	// {"time":"2024-12-02T22:17:38.192361+09:00","level":"INFO","msg":"Info Message","追加メッセージ":"Infoレベルのメッセージです"}
+
+	logger.Info("Info Message", "追加メッセージ")
+	// {"time":"2024-12-02T22:18:28.502302+09:00","level":"INFO","msg":"Info Message","!BADKEY":"追加メッセージ"}
 }
