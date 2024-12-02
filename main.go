91aa945220ea1a8676cac2c9bb884299bf04881d
hrn-tmy <mj0829mj0625@icloud.com>
2024-12-03 00:22:54 +0900

    ネストされたログを出力

--- main.go
+++ main.go
@@ -6,25 +6,8 @@ import (
 )
 
 func main() {
-	// 出力のカスタマイズを行う
-	opt := slog.HandlerOptions{
-		// ログの出力場所を詳述する
-		AddSource: true,
-		// 出力するログレベルを設定
-		Level: slog.LevelWarn,
-		// 追加情報の出力に関する設定
-		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
-			if a.Value.String() == "Warn Message" {
-				a.Value = slog.StringValue("WARN MESSAGE")
-			}
-			return a
-		},
-	}
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, &opt))
-	// Warn以上を出力するように設定したため出力されない
-	logger.Info("Info Message")
-	logger.Warn("Warn Message")
-	// {"time":"2024-12-02T22:29:02.602679+09:00","level":"WARN","source":{"function":"main.main","file":"/Users/hoge/fuga/golang_logs/main.go","line":21},"msg":"WARN MESSAGE"}
-	logger.Error("Error Message")
-	// {"time":"2024-12-02T22:29:02.603285+09:00","level":"ERROR","source":{"function":"main.main","file":"/Users/hoge/fuga/golang_logs/main.go","line":22},"msg":"Error Message"}
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	// slog.Groupでネストされたログを作成する
+	logger.Info("Info Message", slog.Group("グループ1", "追加情報", "Infoレベルです"), slog.Group("グループ2", slog.String("追加情報", "Infoレベルだ")))
+	// {"time":"2024-12-02T22:44:50.577887+09:00","level":"INFO","msg":"Info Message","グループ1":{"追加情報":"Infoレベルです"},"グループ2":{"追加情報":"Infoレベルだ"}}
 }
